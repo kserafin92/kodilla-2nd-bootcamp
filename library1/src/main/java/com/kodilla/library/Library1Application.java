365dcb25d8c086b95e3238305790303bf98cd7d2
serak <serakrzysztof@gmail.com>
2024-09-26 09:38:48 +0200

    security

---
+++ library1/src/main/java/com/kodilla/library/Library1Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Library1Application.class, args);
+    }
+
+}
---
+++ library1/src/main/java/com/kodilla/library/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.kodilla.library.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
